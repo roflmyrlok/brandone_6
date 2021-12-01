cc2ebb4e97ff7b0538fbdb07685dd2818114467d
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-01 10:40:42 +0200

    Update 0-impr.py

--- 0-impr.py
+++ 0-impr.py
@@ -1,5 +1,8 @@
-
+def sum(numbers):
+    result = 0
+    for i in range(len(numbers)):
+        result += int(numbers[i])
+    return result
 numbers_to_use = [32,23,2]
-
 answer = sum(numbers_to_use)
 print(answer)
